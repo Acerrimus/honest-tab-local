fd8e5453b58d82673fcf7db25ab94052299ed31c
Sahil <sahilsingh.contact@gmail.com>
2026-01-31 16:21:13 +0000

    feat: implement more robust .credentials path

--- honest_tab/sheet.py
+++ honest_tab/sheet.py
@@ -1,21 +1,19 @@
+from pathlib import Path
 import gspread
-
-gclient = None
-spreadsheet = None
-user_sheet = None 
-item_sheet = None 
-order_sheet = None
-admin_sheet = None
 
 has_backend = False
 
 try:
-	gclient = gspread.service_account()
-	spreadsheet = gclient.open("HonestTabData")
+	GSPREAD_SERVICE_ACCOUNT_CREDENTIALS_PATH = \
+		Path(".credentials/service_account.json")
+
+	gclient = gspread.service_account(GSPREAD_SERVICE_ACCOUNT_CREDENTIALS_PATH)
+	spreadsheet = gclient.open("Test - OBHonestyData")
 	user_sheet = spreadsheet.worksheet("users")
 	item_sheet = spreadsheet.worksheet("items")
 	order_sheet = spreadsheet.worksheet("orders")
 	admin_sheet = spreadsheet.worksheet("admin")
+
 	has_backend = True
 except:
 	print("Failed to load google sheets: running without backed")
