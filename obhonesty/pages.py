6a70c52c8bfa443e72ab0a4e731614e4a7025944
Sahil <sahilsingh.contact@gmail.com>
2026-02-18 15:44:58 +0000

    chore: remove comments for ci push

--- obhonesty/pages.py
+++ obhonesty/pages.py
@@ -472,27 +472,6 @@ def user_signup_page() -> rx.Component:
                             border_color="tomato" 
                         ),
 
-
-                        # ============= TODO: Implement infuture: =======================
-                        # rx.text("Help us keep you safe. Please distinguish between life-threatening allergies and general sensitivities.", weight="light"),
-
-                        # rx.text("Severe Allergies", weight="bold", color="tomato"),
-                        # rx.text("Requires strict cross-contamination protocols.", size="1", color="gray"),
-                        # rx.input(
-                        #     placeholder="e.g. Peanuts, Shellfish (Anaphylaxis risk)",
-                        #     name="allergies",
-                        #     width="100%",
-                        #     border_color="tomato" # Subtle visual cue for importance
-                        # ),
-
-
-                        # rx.text("Intolerances & Preferences", weight="medium"),
-                        # rx.text("Ingredients to avoid to prevent discomfort.", size="1", color="gray"),
-                        # rx.input(
-                        #     placeholder="e.g. Lactose, Gluten (non-celiac), Onions",
-                        #     name="intolerances",
-                        #     width="100%"
-                        # ),
                         rx.spacer(),
                         rx.button(
                             rx.text("Submit", size=default_button_text_size), type="submit",
@@ -845,15 +824,7 @@ def show_signup(meal: Meal_Model):
             )
         ),
         key=meal.meal_id,
-        bg=rx.cond(
-            has_allergies,
-            "#FC281D20",  # red for allergies
-            rx.cond(
-                has_intolerances,
-                "#FCD31D1F",  # yellow for intolerances
-                "transparent"
-            )
-        )
+        bg=rx.cond(has_allergies, "#FC281D20", "transparent"),
     )
 
 def admin_dinner() -> rx.Component:
