85d540cb88181b10e8cb76578a89655b96ef5ae5
Zac <zac@zacmoss.co.uk>
2026-04-06 11:14:34 +0200

    test: added e2e tests for guests successfully paying theirt tab and either checking out or not checking out

---
+++ e2e/cypress/e2e/paying_tab/current_guest_successful_checkout_paying_tab.cy.js
@@ -0,0 +1,44 @@
+import { getDataTestIdElement } from "../../helpers";
+import {
+  createUserOrderAPI,
+  generateOrderDetails,
+  getUserOrdersAPI,
+} from "../../steps/orders";
+import { createUserAPI, generateUsername, logUserOn } from "../../steps/users";
+
+describe("When a current guest successfully pays their tab and chooses to check out", () => {
+  const username = generateUsername();
+  it("they will no longer be seen on the login screen", () => {
+    createUserAPI(username);
+    const itemNames = [1, 2, 3].map((num) => `REGISTERED TEST ITEM${num}`);
+    const orderDetails = itemNames.map((itemName) =>
+      generateOrderDetails(username, itemName),
+    );
+    cy.wrap(orderDetails).each((orderDetail) => {
+      createUserOrderAPI(orderDetail);
+    });
+    cy.visit("/");
+    logUserOn(username);
+    getDataTestIdElement("pay-tab-button").click();
+    getDataTestIdElement("ordered_item").should("have.lengthOf", 3);
+    getDataTestIdElement("total-amount-due").should(
+      "have.text",
+      "Total amount due: €3.00",
+    );
+    getDataTestIdElement("pay-tab-button").click();
+    getDataTestIdElement("radio-input-yes").click();
+    getDataTestIdElement("submit-button").click();
+    getDataTestIdElement("checkout-complete-text").should("be.visible");
+    getDataTestIdElement("stripe_dialog_close").click();
+    getDataTestIdElement("title").should("be.visible");
+    cy.get(`[data-testid="user-button-${username}"]`).should("not.exist");
+  });
+
+  it("all their orders will have a paid status", () => {
+    cy.waitUntil(() =>
+      getUserOrdersAPI(username).then((response) =>
+        response.body.orders.every((order) => order.paid),
+      ),
+    );
+  });
+});
---
+++ e2e/cypress/e2e/paying_tab/current_guest_successful_not_checking_out_paying_tab.cy.js
@@ -0,0 +1,49 @@
+import { getDataTestIdElement } from "../../helpers";
+import {
+  createUserOrderAPI,
+  generateOrderDetails,
+  getUserOrdersAPI,
+} from "../../steps/orders";
+import { createUserAPI, generateUsername, logUserOn } from "../../steps/users";
+
+describe("When a current guest successfully pays their tab and chooses to not check out", () => {
+  const username = generateUsername();
+  it("they will stay logged in and their paid items will not be in their tab", () => {
+    createUserAPI(username);
+    const itemNames = [1, 2, 3].map((num) => `REGISTERED TEST ITEM${num}`);
+    const orderDetails = itemNames.map((itemName) =>
+      generateOrderDetails(username, itemName),
+    );
+    cy.wrap(orderDetails).each((orderDetail) => {
+      createUserOrderAPI(orderDetail);
+    });
+    cy.visit("/");
+    logUserOn(username);
+    getDataTestIdElement("pay-tab-button").click();
+    getDataTestIdElement("ordered_item").should("have.lengthOf", 3);
+    getDataTestIdElement("total-amount-due").should(
+      "have.text",
+      "Total amount due: €3.00",
+    );
+    getDataTestIdElement("pay-tab-button").click();
+    getDataTestIdElement("radio-input-no").click();
+    getDataTestIdElement("submit-button").click();
+    getDataTestIdElement("stripe_payment_successful_text").should("be.visible");
+    getDataTestIdElement("stripe_dialog_close").click();
+    cy.contains("Tab paid successfully!");
+    getDataTestIdElement("pay-tab-button").click();
+    getDataTestIdElement("total-amount-due").should(
+      "have.text",
+      "Total amount due: €0.00",
+    );
+    getDataTestIdElement("ordered_item").should("not.exist");
+  });
+
+  it("all their orders will have a paid status", () => {
+    cy.waitUntil(() =>
+      getUserOrdersAPI(username).then((response) =>
+        response.body.orders.every((order) => order.paid),
+      ),
+    );
+  });
+});
