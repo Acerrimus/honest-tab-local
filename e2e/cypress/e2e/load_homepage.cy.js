1a73f192b1dbc021c2942d57cb68aa49294c0b0b
Zac <zac@zacmoss.co.uk>
2026-04-22 13:59:14 +0200

    test: removed unnecessary test

--- e2e/cypress/e2e/load_homepage.cy.js
+++
@@ -1,8 +0,0 @@
-import { getDataTestIdElement } from "../helpers";
-
-describe("When loading the homepage", () => {
-  it("loads successfully", () => {
-    cy.visit("/")
-    getDataTestIdElement("title").contains("Olive Branch")
-  });
-});
