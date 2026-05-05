df063d6ad10083ffd9283475b834581416d9ba1e
Zac <zac@zacmoss.co.uk>
2026-05-05 21:20:44 +0200

    test: disabled test isolation for meal e2e tests

--- e2e/cypress/e2e/meals/dinner_guests_tapping_served_will_set_as_served_and_update_count.cy.js
+++ e2e/cypress/e2e/meals/dinner_guests_tapping_served_will_set_as_served_and_update_count.cy.js
@@ -1,76 +1,76 @@
 import { getDataTestIdElement } from "../../helpers";
-import {
-  getTotalServedInt,
-  waitForReceiverServedStatusAndTotalCountToRender,
-} from "../../steps/meals";
+import { getTotalServedInt } from "../../steps/meals";
 import { createDinnerOrderApi, generateReceiver } from "../../steps/orders";
 import { createGuestUserApi, generateUsername } from "../../steps/users";
 
-describe("When an admin user clicks served for a guest's dinner meal", () => {
-  const username = generateUsername();
-  const receiver = generateReceiver(username)
+describe(
+  "When an admin user clicks served for a guest's dinner meal",
+  { testIsolation: false },
+  () => {
+    const username = generateUsername();
+    const receiver = generateReceiver(username);
 
-  it("it will show as served and increment the guests served count with the volunteers served count staying the same", function () {
-    createGuestUserApi(username);
-    createDinnerOrderApi(username, receiver);
-    cy.visit("/admin/dinner");
-    cy.contains(receiver, {timeout: 60000});
-    getDataTestIdElement("total-served").invoke("text").as("totalServed");
-    getDataTestIdElement("total-guests-served")
-      .invoke("text")
-      .as("totalGuestsServed");
-    getDataTestIdElement("total-volunteers-served")
-      .invoke("text")
-      .as("totalVolunteersServed");
-    getDataTestIdElement("meal-row")
-      .filter(`:contains(${receiver})`)
-      .find(`[data-testid=served-button]`)
-      .then(($servedButtonEl) => {
-        expect($servedButtonEl).to.have.text("✖️");
-        cy.wrap($servedButtonEl).click().should("have.text", "✅");
-        getDataTestIdElement("total-served").should(
-          "have.text",
-          `Total served: ${getTotalServedInt(this.totalServed) + 1}`,{timeout: 60000}
-        );
-        getDataTestIdElement("total-volunteers-served").should(
-          "have.text",
-          this.totalVolunteersServed,
-        );
-        getDataTestIdElement("total-guests-served").should(
-          "have.text",
-          `Total served: ${getTotalServedInt(this.totalGuestsServed) + 1}`,
-        );
-      });
-  });
+    it("it will show as served and increment the guests served count with the volunteers served count staying the same", function () {
+      createGuestUserApi(username);
+      createDinnerOrderApi(username, receiver);
+      cy.visit("/admin/dinner");
+      cy.contains(receiver, { timeout: 60000 });
+      getDataTestIdElement("total-served").invoke("text").as("totalServed");
+      getDataTestIdElement("total-guests-served")
+        .invoke("text")
+        .as("totalGuestsServed");
+      getDataTestIdElement("total-volunteers-served")
+        .invoke("text")
+        .as("totalVolunteersServed");
+      getDataTestIdElement("meal-row")
+        .filter(`:contains(${receiver})`)
+        .find(`[data-testid=served-button]`)
+        .then(($servedButtonEl) => {
+          expect($servedButtonEl).to.have.text("✖️");
+          cy.wrap($servedButtonEl).click().should("have.text", "✅");
+          getDataTestIdElement("total-served").should(
+            "have.text",
+            `Total served: ${getTotalServedInt(this.totalServed) + 1}`,
+            { timeout: 60000 },
+          );
+          getDataTestIdElement("total-volunteers-served").should(
+            "have.text",
+            this.totalVolunteersServed,
+          );
+          getDataTestIdElement("total-guests-served").should(
+            "have.text",
+            `Total served: ${getTotalServedInt(this.totalGuestsServed) + 1}`,
+          );
+        });
+    });
 
-  it("and when clicked again it will show as not served and decrement the guests served count with the volunteers served count staying the same", function () {
-    cy.visit("/admin/dinner");
-    waitForReceiverServedStatusAndTotalCountToRender(receiver);
-    getDataTestIdElement("total-served").invoke("text").as("totalServed");
-    getDataTestIdElement("total-guests-served")
-      .invoke("text")
-      .as("totalGuestsServed");
-    getDataTestIdElement("total-volunteers-served")
-      .invoke("text")
-      .as("totalVolunteersServed");
-    getDataTestIdElement("meal-row")
-      .filter(`:contains(${receiver})`)
-      .find(`[data-testid=served-button]`)
-      .then(($servedButtonEl) => {
-        expect($servedButtonEl).to.have.text("✅");
-        cy.wrap($servedButtonEl).click().should("have.text", "✖️");
-        getDataTestIdElement("total-served").should(
-          "have.text",
-          `Total served: ${getTotalServedInt(this.totalServed) - 1}`,
-        );
-        getDataTestIdElement("total-volunteers-served").should(
-          "have.text",
-          this.totalVolunteersServed,
-        );
-        getDataTestIdElement("total-guests-served").should(
-          "have.text",
-          `Total served: ${getTotalServedInt(this.totalGuestsServed) - 1}`,
-        );
-      });
-  });
-});
+    it("and when clicked again it will show as not served and decrement the guests served count with the volunteers served count staying the same", function () {
+      getDataTestIdElement("total-served").invoke("text").as("totalServed");
+      getDataTestIdElement("total-guests-served")
+        .invoke("text")
+        .as("totalGuestsServed");
+      getDataTestIdElement("total-volunteers-served")
+        .invoke("text")
+        .as("totalVolunteersServed");
+      getDataTestIdElement("meal-row")
+        .filter(`:contains(${receiver})`)
+        .find(`[data-testid=served-button]`)
+        .then(($servedButtonEl) => {
+          expect($servedButtonEl).to.have.text("✅");
+          cy.wrap($servedButtonEl).click().should("have.text", "✖️");
+          getDataTestIdElement("total-served").should(
+            "have.text",
+            `Total served: ${getTotalServedInt(this.totalServed) - 1}`,
+          );
+          getDataTestIdElement("total-volunteers-served").should(
+            "have.text",
+            this.totalVolunteersServed,
+          );
+          getDataTestIdElement("total-guests-served").should(
+            "have.text",
+            `Total served: ${getTotalServedInt(this.totalGuestsServed) - 1}`,
+          );
+        });
+    });
+  },
+);
--- e2e/cypress/e2e/meals/dinner_volunteers_tapping_served_will_set_as_served_and_update_count.cy.js
+++ e2e/cypress/e2e/meals/dinner_volunteers_tapping_served_will_set_as_served_and_update_count.cy.js
@@ -1,75 +1,74 @@
 import { getDataTestIdElement } from "../../helpers";
-import {
-  getTotalServedInt,
-  waitForReceiverServedStatusAndTotalCountToRender,
-} from "../../steps/meals";
+import { getTotalServedInt } from "../../steps/meals";
 import { generateReceiver } from "../../steps/orders";
 import { createVolunteerUserApi, generateUsername } from "../../steps/users";
 
-describe("When an admin user clicks served for a volunteer's dinner meal", () => {
-  const username = generateUsername();
-  const receiver = generateReceiver(username)
+describe(
+  "When an admin user clicks served for a volunteer's dinner meal",
+  { testIsolation: false },
+  () => {
+    const username = generateUsername();
+    const receiver = generateReceiver(username);
 
-  it("it will show as served and increment the volunteers served count with the guests served staying the same", function () {
-    createVolunteerUserApi(username);
-    cy.visit("/admin/dinner");
-    cy.contains(receiver, { timeout: 60000 });
-    getDataTestIdElement("total-served").invoke("text").as("totalServed");
-    getDataTestIdElement("total-guests-served")
-      .invoke("text")
-      .as("totalGuestsServed");
-    getDataTestIdElement("total-volunteers-served")
-      .invoke("text")
-      .as("totalVolunteersServed");
-    getDataTestIdElement("meal-row")
-      .filter(`:contains(${receiver})`)
-      .find(`[data-testid=served-button]`)
-      .then(($servedButtonEl) => {
-        expect($servedButtonEl).to.have.text("✖️");
-        cy.wrap($servedButtonEl).click().should("have.text", "✅");
-        getDataTestIdElement("total-served").should(
-          "have.text",
-          `Total served: ${getTotalServedInt(this.totalServed) + 1}`,
-        );
-        getDataTestIdElement("total-guests-served").should(
-          "have.text",
-          this.totalGuestsServed,
-        );
-        getDataTestIdElement("total-volunteers-served").should(
-          "have.text",
-          `Total served: ${getTotalServedInt(this.totalVolunteersServed) + 1}`,
-        );
-      });
-  });
+    it("it will show as served and increment the volunteers served count with the guests served staying the same", function () {
+      createVolunteerUserApi(username);
+      cy.visit("/admin/dinner");
+      cy.contains(receiver, { timeout: 60000 });
+      getDataTestIdElement("total-served").invoke("text").as("totalServed");
+      getDataTestIdElement("total-guests-served")
+        .invoke("text")
+        .as("totalGuestsServed");
+      getDataTestIdElement("total-volunteers-served")
+        .invoke("text")
+        .as("totalVolunteersServed");
+      getDataTestIdElement("meal-row")
+        .filter(`:contains(${receiver})`)
+        .find(`[data-testid=served-button]`)
+        .then(($servedButtonEl) => {
+          expect($servedButtonEl).to.have.text("✖️");
+          cy.wrap($servedButtonEl).click().should("have.text", "✅");
+          getDataTestIdElement("total-served").should(
+            "have.text",
+            `Total served: ${getTotalServedInt(this.totalServed) + 1}`,
+          );
+          getDataTestIdElement("total-guests-served").should(
+            "have.text",
+            this.totalGuestsServed,
+          );
+          getDataTestIdElement("total-volunteers-served").should(
+            "have.text",
+            `Total served: ${getTotalServedInt(this.totalVolunteersServed) + 1}`,
+          );
+        });
+    });
 
-  it("and when clicked again it will show as not served and decrement the volunteers served count with the guests served count staying the same", function () {
-    cy.visit("/admin/dinner");
-    waitForReceiverServedStatusAndTotalCountToRender(receiver);
-    getDataTestIdElement("total-served").invoke("text").as("totalServed");
-    getDataTestIdElement("total-volunteers-served")
-      .invoke("text")
-      .as("totalVolunteersServed");
-    getDataTestIdElement("total-guests-served")
-      .invoke("text")
-      .as("totalGuestsServed");
-    getDataTestIdElement("meal-row")
-      .filter(`:contains(${receiver})`)
-      .find(`[data-testid=served-button]`)
-      .then(($servedButtonEl) => {
-        expect($servedButtonEl).to.have.text("✅");
-        cy.wrap($servedButtonEl).click().should("have.text", "✖️");
-        getDataTestIdElement("total-served").should(
-          "have.text",
-          `Total served: ${getTotalServedInt(this.totalServed) - 1}`,
-        );
-        getDataTestIdElement("total-guests-served").should(
-          "have.text",
-          this.totalGuestsServed,
-        );
-        getDataTestIdElement("total-volunteers-served").should(
-          "have.text",
-          `Total served: ${getTotalServedInt(this.totalVolunteersServed) - 1}`,
-        );
-      });
-  });
-});
+    it("and when clicked again it will show as not served and decrement the volunteers served count with the guests served count staying the same", function () {
+      getDataTestIdElement("total-served").invoke("text").as("totalServed");
+      getDataTestIdElement("total-volunteers-served")
+        .invoke("text")
+        .as("totalVolunteersServed");
+      getDataTestIdElement("total-guests-served")
+        .invoke("text")
+        .as("totalGuestsServed");
+      getDataTestIdElement("meal-row")
+        .filter(`:contains(${receiver})`)
+        .find(`[data-testid=served-button]`)
+        .then(($servedButtonEl) => {
+          expect($servedButtonEl).to.have.text("✅");
+          cy.wrap($servedButtonEl).click().should("have.text", "✖️");
+          getDataTestIdElement("total-served").should(
+            "have.text",
+            `Total served: ${getTotalServedInt(this.totalServed) - 1}`,
+          );
+          getDataTestIdElement("total-guests-served").should(
+            "have.text",
+            this.totalGuestsServed,
+          );
+          getDataTestIdElement("total-volunteers-served").should(
+            "have.text",
+            `Total served: ${getTotalServedInt(this.totalVolunteersServed) - 1}`,
+          );
+        });
+    });
+  },
+);
