ae0a92c3ca0ead4cd24533bb522fca8aa7f6b87b
Zac <zac@zacmoss.co.uk>
2026-05-07 13:55:49 +0200

    chore: removed unused functions

--- obhonesty/aux.py
+++ obhonesty/aux.py
@@ -1,7 +1,5 @@
-import re
 import random
 import string
-from typing import Any, Optional
 import socket
 from datetime import datetime
 from reflex.vars import NumberVar, var_operation, var_operation_return
@@ -18,28 +16,11 @@ def two_decimal_points(value: NumberVar):
     )
 
 
-def safe_float_convert(s: str) -> Optional[float]:
-    try:
-        return float(s)
-    except ValueError:
-        return None
-    except TypeError:
-        return None
-
-
-def value_or(x: Optional[Any], default: Any) -> Any:
-    return x if not x is None else default
-
-
 _alphabet = string.ascii_lowercase + string.digits
 
 
 def short_uid(k=8):
     return "".join(random.choices(_alphabet, k=8))
-
-
-def lower_non_alpha_num(s: str) -> str:
-    return re.sub(r"\W+", "", s).lower()
 
 
 def check_internet_connection():
